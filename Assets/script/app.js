e0e7e0c73565aeb6d593211a9d00e5c7889272a8
Ken <kensindytomjerry@gmail.com>
2023-11-10 20:56:42 -0600

    finishing submit
    almost finished all design. Need to be update in the future for improvement.

--- Assets/script/app.js
+++ Assets/script/app.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const btnSend = document.querySelector('.sendBtn');
+btnSend.addEventListener('click', () => {
+    const nameText = document.querySelector('.nameText');
+    const emailText = document.querySelector('.eMailText');
+    const multilineInput = document.querySelector('.multilineInput');
+    nameText.value = '';
+    emailText.value = '';
+    multilineInput.value = '';
+    Swal.fire({
+        title: 'Your message has been sent!',
+        icon: 'info',
+        confirmButtonText: 'OK'
+      });
+});
